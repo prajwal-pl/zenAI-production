9cc8487e5cfa1da830f7f9b916fd580ba43333ff
Prajwal-PL <prajwalpl096@gmail.com>
2024-10-06 20:57:09 +0530

    initialize drizzle ORM and neon postgers database

---
+++ drizzle.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from "drizzle-kit";
+// import * as dotenv from "dotenv";
+// dotenv.config({ path: ".env" });
+
+export default {
+  out: "./src/lib/db",
+  schema: "./src/lib/db/schema.ts",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+} satisfies Config;
+
+// npx drizzle-kit push:pg
