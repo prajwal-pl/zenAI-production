e92d7ee0e7755f9f2e689442f9163900afd18c94
Prajwal-PL <prajwalpl096@gmail.com>
2024-11-03 18:43:02 +0530

    Successfully added speech to text functionality

--- src/app/(main)/dashboard/interview/[interviewId]/questions/components/VideoCam.tsx
+++ src/app/(main)/dashboard/interview/[interviewId]/questions/components/VideoCam.tsx
@@ -1,9 +1,12 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import { Video, VideoOff, WebcamIcon } from "lucide-react";
-import React from "react";
+import { MicIcon, MicOff, Video, VideoOff, WebcamIcon } from "lucide-react";
+import React, { useCallback, useEffect, useState } from "react";
 import useSpeechToText from "react-hook-speech-to-text";
 import Webcam from "react-webcam";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
 
 type Props = {
   questions: any[];
@@ -11,19 +14,38 @@ type Props = {
 
 const VideoCam = ({ questions }: Props) => {
   const [enableWebcam, setEnableWebcam] = React.useState(false);
+  const [isRecording, setIsRecording] = useState(false);
+  const [transcripts, setTranscripts] = useState<string[]>([]);
   const {
-    error,
-    interimResult,
-    isRecording,
-    results,
-    startSpeechToText,
-    stopSpeechToText,
-  } = useSpeechToText({
-    continuous: true,
-    useLegacyResults: false,
-  });
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition,
+  } = useSpeechRecognition();
+
+  useEffect(() => {
+    if (transcript) {
+      setTranscripts((prev) => [...prev, transcript]);
+      console.log(transcripts);
+    }
+  }, [transcript]);
+
+  const handleRecording = useCallback(() => {
+    if (!isRecording) {
+      setIsRecording(true);
+      SpeechRecognition.startListening({ continuous: true });
+    } else {
+      setIsRecording(false);
+      SpeechRecognition.stopListening();
+    }
+  }, [isRecording]);
+
+  if (!browserSupportsSpeechRecognition) {
+    return <span>Browser doesn't support speech recognition.</span>;
+  }
+
   return (
-    <div className="">
+    <div className="flex items-center justify-center flex-col">
       <div className="p-5 border rounded-lg">
         <div className="relative">
           {enableWebcam ? (
@@ -40,7 +62,7 @@ const VideoCam = ({ questions }: Props) => {
               mirrored
             />
           ) : (
-            <div className="flex items-center justify-center rounded-lg bg-secondary w-full h-[15rem] md:h-[25rem] ">
+            <div className="flex items-center justify-center rounded-lg bg-secondary md:w-[35rem] w-[18rem] h-[15rem] md:h-[24rem] ">
               <WebcamIcon className="w-24 h-24" />
             </div>
           )}
@@ -60,6 +82,20 @@ const VideoCam = ({ questions }: Props) => {
           </Button>
         </div>
       </div>
+      <Button onClick={handleRecording} className=" m-2">
+        {listening ? (
+          <div className="flex items-center gap-2">
+            <MicOff className="w-4 h-4" />
+            Stop Recording
+          </div>
+        ) : (
+          <div className="flex items-center gap-2">
+            <MicIcon className="w-4 h-4" />
+            Start Recording
+          </div>
+        )}
+      </Button>
+      <p>{transcript}</p>
     </div>
   );
 };
